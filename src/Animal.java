5b547f75398c1de5d3ed68b2b1a3368041c246e4
morav <mmatikm@gmail.com>
2024-03-08 13:13:10 +0100

    Editing animals

--- src/Animal.java
+++ src/Animal.java
@@ -15,18 +15,41 @@ public abstract class Animal {
                 '}';
     }
 
-    public static class Salmon{
+    public Animal(String type, String name, Size size, String specialCare) {
+        this.type = type;
+        this.name = name;
+        this.size = size;
+        this.specialCare = specialCare;
     }
 
-    public static class Shrimp {
+    public static class Salmon extends Animal{
+        public Salmon(String type, String name, Size size, String specialCare) {
+            super(type, name, size, specialCare);
+
+        }
     }
 
-    public static class Leech {
+    public static class Shrimp extends Animal{
+        public Shrimp(String type, String name, Size size, String specialCare) {
+            super(type, name, size, specialCare);
+        }
     }
 
-    public static class Dragonfly {
+    public static class Leech extends Animal{
+        public Leech(String type, String name, Size size, String specialCare) {
+            super(type, name, size, specialCare);
+        }
     }
 
-    public static class Carp {
+    public static class Dragonfly extends Animal{
+        public Dragonfly(String type, String name, Size size, String specialCare) {
+            super(type, name, size, specialCare);
+        }
+    }
+
+    public static class Carp extends Animal{
+        public Carp(String type, String name, Size size, String specialCare) {
+            super(type, name, size, specialCare);
+        }
     }
 }
