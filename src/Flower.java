9144a5226d8b1048cac9509e0af1c140e29b11be
morav <mmatikm@gmail.com>
2024-03-08 13:26:09 +0100

    Adding flowers

--- src/Flower.java
+++ src/Flower.java
@@ -15,4 +15,40 @@ public abstract class Flower {
                 '}';
     }
 
+    public Flower(String name, double price, double neededArea, double chanceOfGrowth) {
+        this.name = name;
+        this.price = price;
+        this.neededArea = neededArea;
+        this.chanceOfGrowth = chanceOfGrowth;
+    }
+
+    public static class Violet extends Flower{
+        public Violet(String name, double price, double neededArea, double chanceOfGrowth) {
+            super(name, price, neededArea, chanceOfGrowth);
+        }
+    }
+
+    public static class Tulip extends Flower {
+        public Tulip(String name, double price, double neededArea, double chanceOfGrowth) {
+            super(name, price, neededArea, chanceOfGrowth);
+        }
+    }
+
+    public static class Rose extends Flower{
+        public Rose(String name, double price, double neededArea, double chanceOfGrowth) {
+            super(name, price, neededArea, chanceOfGrowth);
+        }
+    }
+
+    public static class Hyacint extends Flower{
+        public Hyacint(String name, double price, double neededArea, double chanceOfGrowth) {
+            super(name, price, neededArea, chanceOfGrowth);
+        }
+    }
+
+    public static class Orchideae extends Flower{
+        public Orchideae(String name, double price, double neededArea, double chanceOfGrowth) {
+            super(name, price, neededArea, chanceOfGrowth);
+        }
+    }
 }
